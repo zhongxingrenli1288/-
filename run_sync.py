270596b13c73a31518ffe7ea480babfd833bfd61
zhongxingrenli1288 <zhongxingrenli1288@gmail.com>
2026-04-19 20:57:44 +0800

    Create run_sync.py

---
+++ run_sync.py
@@ -0,0 +1,6 @@
+from booking_sync import sync_booking_ical
+
+sync_booking_ical("https://ical.booking.com/v1/export?t=0152fb10-fb36-4b80-bd14-5c59e334219e", "201")
+sync_booking_ical("https://ical.booking.com/v1/export?t=ea4ac508-6d9b-4f4f-9ded-1f133669c632", "202")
+sync_booking_ical("https://ical.booking.com/v1/export?t=f2b842e9-de1a-4092-a28d-8a911d0ebd6d", "301")
+sync_booking_ical("https://ical.booking.com/v1/export?t=eed8891e-2a9e-4ad1-89cf-7ef75ae31704", "302")
